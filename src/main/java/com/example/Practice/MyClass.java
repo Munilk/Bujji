6414551ae8dc578ad50e420df72cec0d94b7eaa4
Munilk <munilk631@gmail.com>
2025-08-16 22:32:47 +0530

    changes

---
+++ src/main/java/com/example/Practice/MyClass.java
@@ -0,0 +1,17 @@
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MyClass{
+
+@GetMapping("abc")
+public  String sayHello(){
+return "hello i am working fine now .......*@#₹__₹₹+₹)_-_(+_(_)₹(+ ";
+
+}
+
+
+}
