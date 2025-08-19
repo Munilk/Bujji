bb4ba5111790083b7ca2589899e8b184c3938bf6
Munilk <munilk631@gmail.com>
2025-08-19 14:35:10 +0530

    changes in Myclass.java

--- src/main/java/com/example/Practice/MyClass.java
+++ src/main/java/com/example/Practice/MyClass.java
@@ -1,16 +1,27 @@
-
+package com.example.Practice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.*;
 
 @RestController
-
+@RequestMapping("home")
 public class MyClass{
 
-@GetMapping("abc")
-public  String sayHello(){
-return "hello i am working fine now .......*@#₹__₹₹+₹)_-_(+_(_)₹(+ ";
 
+@AutoWired
+@GetMapping("greet")
+public  String sayHello(){
+return "hello i am working fine now ...in abc get mapping";
+
+}
+@PostMapping("add")
+public String add(@RequestBody String s){
+return "hello add student is working "+" : "+s;
 }
 
 
