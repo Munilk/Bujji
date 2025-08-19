c55001c3e9c4320b03b7df6d35771043961d9626
Munilk <munilk631@gmail.com>
2025-08-19 18:27:42 +0530

    change in myclass

--- src/main/java/com/example/Practice/MyClass.java
+++ src/main/java/com/example/Practice/MyClass.java
@@ -1,5 +1,29 @@
 package com.example.Practice;
 
+
+
+
+
+/*
+
+/data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[5,8] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[8,9] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[9,8] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[10,11] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[11,1] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[12,8] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[12,30] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[13,8] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[13,36] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[14,8] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[14,37] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[15,8] class, interface, enum, or record expected
+[ERROR] /data/data/com.termux/files/home/downloads/Practice/src/main/java/com/example/Practice/Student.java:[15,43] class, interface, enum, or record expected
+
+
+*/
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +31,15 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.*;
+import com.example.Practice.*;
 
 @RestController
 @RequestMapping("home")
 public class MyClass{
 
 
-@AutoWired
+//@AutoWired
+//private Student s;
 @GetMapping("greet")
 public  String sayHello(){
 return "hello i am working fine now ...in abc get mapping";
